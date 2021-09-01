3a9ab34055802f3fddb40d93f18cb57c50191f9b
yujinpan <1192878390@qq.com>
2021-09-01 10:22:48 +0800

    Update shims-tsx.d.ts

--- src/shims-tsx.d.ts
+++ src/shims-tsx.d.ts
@@ -9,5 +9,10 @@ declare global {
     interface IntrinsicElements {
       [elem: string]: any;
     }
+
+    // https://github.com/vuejs/vue-cli/issues/2417
+    interface ElementAttributesProperty {
+      $props: {};
+    }
   }
 }
