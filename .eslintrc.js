96198b15d5953d09ec01aeb97de59bdda3f20ddd
yujinpan <1192878390@qq.com>
2022-08-17 11:40:09 +0800

    ban `vue/multi-word-component-names`

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,6 +24,7 @@ module.exports = {
         semi: true,
       },
     ],
+    'vue/multi-word-component-names': 'off',
     'no-debugger': productionError,
     'no-console': productionError,
     eqeqeq: ['error', 'always'],
