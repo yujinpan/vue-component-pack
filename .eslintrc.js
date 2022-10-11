ff95e66983f6f886585b7c650578d5e138251349
yujinpan <1192878390@qq.com>
2022-10-11 10:36:11 +0800

    fix: vue2 should be `plugin:vue/essential`

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,7 @@ const path = require('path');
 module.exports = {
   root: true,
   extends: [
-    'plugin:vue/vue3-essential',
+    'plugin:vue/essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript/recommended',
     '@vue/eslint-config-prettier',
