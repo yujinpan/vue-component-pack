3ff695e17539fb9aecefdc3af02a8c33af392b23
yujinpan <1192878390@qq.com>
2022-10-17 14:04:54 +0800

    fix: scss alias path resolve error

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 /// <reference types="vitest" />
+
 import vue2 from '@vitejs/plugin-vue2';
 import vue2Jsx from '@vitejs/plugin-vue2-jsx';
-import { fileURLToPath } from 'url';
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
+
+const alias = {
+  '@': resolve('src'),
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -18,19 +23,16 @@ export default defineConfig({
     }),
   ],
   resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('src', import.meta.url)),
-    },
+    alias,
+    extensions: ['.vue', '.js', '.ts', '.jsx', '.tsx', '.json'],
   },
   css: {
     preprocessorOptions: {
       scss: {
-        // ignore sass warnings for "10px / 2px"
-        quietDeps: true,
         // resolve start path for "~", like: "~external/style/var.scss"
         importer: (url: string) => {
           return {
-            file: url.startsWith('~') ? url.slice(1) : url,
+            file: parseAlias(url.startsWith('~') ? url.slice(1) : url, alias),
           };
         },
       },
@@ -41,3 +43,17 @@ export default defineConfig({
     dir: 'src',
   },
 });
+
+function parseAlias(file: string, alias: Record<string, string>) {
+  const key = Object.keys(alias).find(
+    (item) => file.startsWith(item + '/') || file === item,
+  );
+  if (key) {
+    if (key === file) {
+      return alias[key];
+    } else {
+      return file.replace(key, alias[key]);
+    }
+  }
+  return file;
+}
